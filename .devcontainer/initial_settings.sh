e086772ffda172dc03323cf7af6360d6f2c1344c
Takashi Furuhashi <tf@yorozu-sys.net>
2023-03-02 11:07:39 +0000

    update.

--- .devcontainer/initial_settings.sh
+++ .devcontainer/initial_settings.sh
@@ -2,8 +2,8 @@
 mkdir /home/vscode/.ssh
 
 #githubのcodespace secretにSSH_PUBKEYを定義しておくこと。
-/usr/bin/gpg -d --batch --passphrase=$GPG_PASSPHRASE /root/tmp/id_rsa.gpg > /home/vscode/.ssh/id_rsa
-echo /root/tmp/id_rsa.pub > /home/vscode/.ssh/id_rsa.pub
+/usr/bin/gpg -d --batch --passphrase=$GPG_PASSPHRASE /cs_work/id_rsa.gpg > /home/vscode/.ssh/id_rsa
+echo /cs_work/id_rsa.pub > /home/vscode/.ssh/id_rsa.pub
 
 /bin/chown vscode:vscode /home/vscode/.ssh/id_rsa
 /bin/chmod 400 /home/vscode/.ssh/id_rsa

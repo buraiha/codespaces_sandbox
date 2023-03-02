48a43b2c1b0c8a4b64db8594e40a6893e60cca28
Takashi Furuhashi <tf@yorozu-sys.net>
2023-03-02 11:30:13 +0000

    update.

--- .devcontainer/initial_settings.sh
+++ .devcontainer/initial_settings.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+echo -n "Starting initial_settings.sh. running user: " && echo $(whoami)
 mkdir /home/vscode/.ssh
 
 #githubのcodespace secretにSSH_PUBKEYを定義しておくこと。
 /usr/bin/gpg -d --batch --passphrase=$GPG_PASSPHRASE /cs_work/id_rsa.gpg > /home/vscode/.ssh/id_rsa
-echo /cs_work/id_rsa.pub > /home/vscode/.ssh/id_rsa.pub
+rm /cs_work/id_rsa.gpg
+mv /cs_work/id_rsa.pub > /home/vscode/.ssh/id_rsa.pub
 
 /bin/chown vscode:vscode /home/vscode/.ssh/id_rsa
 /bin/chmod 400 /home/vscode/.ssh/id_rsa
 /bin/chown vscode:vscode /home/vscode/.ssh/id_rsa.pub
 /bin/chmod 644 /home/vscode/.ssh/id_rsa.pub
 
-echo -n "running user: " && echo $(whoami)
-echo "initial_settings.sh done."
+echo "initial_settings.sh is done."

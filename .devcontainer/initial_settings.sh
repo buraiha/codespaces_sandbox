4a4e301c444b14fc27e8d16a419fbbc3536adbc8
Takashi Furuhashi <tf@yorozu-sys.net>
2023-03-16 09:37:58 +0000

    update.

--- .devcontainer/initial_settings.sh
+++ .devcontainer/initial_settings.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo -n "Starting initial_settings.sh. running user: " && echo $(whoami)
-mkdir /home/vscode/.ssh
 
 # 本コンテナで使うssh鍵ペアの復号&設置処理。
 # 本コンテナから外部サーバ等へssh突する場合に必要。
@@ -12,6 +11,7 @@ mkdir /home/vscode/.ssh
 
 # ここから↓
 # id_rsa復号処理 -> .sshディレクトリ設置処理
+mkdir /home/vscode/.ssh
 /usr/bin/gpg -d --batch --passphrase=$GPG_PASSPHRASE /cs_work/id_rsa.gpg > /home/vscode/.ssh/id_rsa
 rm /cs_work/id_rsa.gpg
 mv /cs_work/id_rsa.pub /home/vscode/.ssh/id_rsa.pub
